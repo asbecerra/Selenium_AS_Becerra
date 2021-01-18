e3b9ef459d2a5aabfd80b5e2073d0ceaff92f86f
AsBT <ansobete@gmail.com>
2021-01-18 16:32:40 -0500

    exercise 1 week 10

--- Semana10/src/test/java/Clase10/Exercise1.java
+++ Semana10/src/test/java/Clase10/Exercise1.java
@@ -1,5 +1,6 @@
 package Clase10;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -16,6 +17,16 @@ public class Exercise1 {
     public void forgotAccountTest(){
         WebDriver driver = getDriver("https://www.facebook.com");
         System.out.println("Title: " + driver.getTitle());
-    }
+        driver.findElement(By.linkText("Forgot Password?")).click();
+        System.out.println("Tilte: " + driver.getTitle());
+        String title = driver.getTitle();
+        String url = driver.getCurrentUrl();
 
+        if (title.equals("Forgot Password | Can't Log In | Facebook") && (!url.equals("https://www.facebook.com"))){
+            System.out.println("Test passed");
+        } else {
+            System.out.println("test failed");
+        }
+        driver.close();
+    }
 }
