b52179710434a8337f7d6c92f1c82e00519706e3
AsBT <ansobete@gmail.com>
2021-01-18 21:04:45 -0500

    exercise 6 and gender selection on exercise 4

--- Semana10/src/test/java/Clase10/Exercise1.java
+++ Semana10/src/test/java/Clase10/Exercise1.java
@@ -2,9 +2,12 @@ package Clase10;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.util.List;
 
 public class Exercise1 {
     public WebDriver getDriver(String URL) {
@@ -49,4 +52,56 @@ public class Exercise1 {
 
         driver.close();
     }
+
+    @Test
+    public void registrationTest() throws InterruptedException {
+        WebDriver driver = getDriver("https://www.facebook.com");
+        System.out.println("Title: " + driver.getTitle());
+        driver.findElement(By.linkText("Create New Account")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.name("firstname")).sendKeys("Alan");
+        driver.findElement(By.name("lastname")).sendKeys("Smith");
+        //dropdown locators
+        WebElement month = driver.findElement(By.id("month"));
+        Select comboMonth = new Select(month);
+        comboMonth.selectByValue("4");
+        WebElement days = driver.findElement(By.id("day"));
+        Select comboDays = new Select(days);
+        comboDays.selectByValue("4");
+        WebElement year = driver.findElement(By.id("year"));
+        Select comboYear = new Select(year);
+        comboYear.selectByVisibleText("2012");
+        List<WebElement> sexRadioButtons = driver.findElements(By.name("sex"));
+        WebElement maleRadioButton = null;
+        for(WebElement element: sexRadioButtons){
+            System.out.println(element.getAttribute("value"));
+            if(element.getAttribute("value").equals("2")){
+                maleRadioButton = element;
+            }
+        }
+        maleRadioButton.click();
+
+        //driver.close();
+    }
+    @Test
+    public void birthDateTest () throws InterruptedException {
+        WebDriver driver = getDriver("https://www.facebook.com");
+        System.out.println("Title: " + driver.getTitle());
+        driver.findElement(By.linkText("Create New Account")).click();
+        Thread.sleep(3000);
+        WebElement month = driver.findElement(By.id("month"));
+        Select comboMonth = new Select(month);
+        comboMonth.selectByValue("12");
+        WebElement days = driver.findElement(By.id("day"));
+        Select comboDays = new Select(days);
+        comboDays.selectByValue("20");
+        WebElement year = driver.findElement(By.id("year"));
+        Select comboYear = new Select(year);
+        comboYear.selectByVisibleText("1990");
+
+        driver.close();
+    }
+
+    @Test
+
 }
